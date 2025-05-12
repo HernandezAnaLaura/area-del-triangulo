8bbb33f4eb69de89b83336dfe61433fc604af4be
Hernandez ana laura <ana.lupsi09@gmail.com>
2025-05-12 16:43:17 -0600

    Add files via upload

---
+++ area de triangulo.py
@@ -0,0 +1,8 @@
+
+base = float(input("Ingresa la base del triángulo: "))
+altura = float(input("Ingresa la altura del triángulo: "))
+
+
+area = (base * altura) / 2
+
+print(f"El área del triángulo es: {area:.2f}.")
